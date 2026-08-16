6066d1e7f329978d1a3eac5ca8780a0c1e84210c
kamga youadeu duprel bryant <byouadeu@gmail.com>
2026-08-16 14:59:03 +0200

    Ajout URL frontend Render dans CORS

--- src/main/java/com/example/recensement/config/CorsConfig.java
+++ src/main/java/com/example/recensement/config/CorsConfig.java
@@ -7,17 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://recensement-frontend.onrender.com"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
     }
-
 }
